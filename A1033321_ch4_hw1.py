36146e490ce55873eac8f52f4497bd4d0485b2bf
yingtonglang <yingtonglang@users.noreply.github.com>
2016-03-29 22:00:44 +0800

    week6 homework

---
+++ A1033321_ch4_hw1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+str = ", you are always on my mind. Your love makes me blind .When I first met you I knew it was a sign\n"
+girlfriends = {'Mary':'Honey','Lynn':'Baby','Jannie':'Little sweetie'}
+print girlfriends['Mary'] + str
+print girlfriends['Lynn'] + str
+print girlfriends['Jannie'] + str
